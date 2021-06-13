69145a8347e554b51ff96400882a23de2da799ca
anupama cv <anu.anupamacv@gmail.com>
2021-06-13 11:45:44 +0530

    ADDED: a board of size 10 to play tic tac toe game

--- src/main/java/com/bridgelabz/TicTacToeGameBuilder.java
+++ src/main/java/com/bridgelabz/TicTacToeGameBuilder.java
@@ -1,6 +1,8 @@
 package com.bridgelabz;
 
 public class TicTacToeGameBuilder {
+    char[] board = new char[10];
+
     public static void main(String[] args) {
         System.out.println("Welcome to Tic tac toe game");
     }
